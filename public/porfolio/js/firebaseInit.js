1f48bd168fd8b43fc37d2dff6828196287c01635
egonz993 <egonz993@gmail.com>
2020-12-11 11:10:43 -0500

    20201211_1110

---
+++ public/porfolio/js/firebaseInit.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyCwG7QgvHbhXtebtxKkYBT03yn1TQ2DI-0",
+  authDomain: "egonzalez.firebaseapp.com",
+  projectId: "egonzalez",
+  storageBucket: "egonzalez.appspot.com",
+  messagingSenderId: "1011692824417",
+  appId: "1:1011692824417:web:e91b3755f80261af678720",
+  measurementId: "G-S3FTEJV997"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
